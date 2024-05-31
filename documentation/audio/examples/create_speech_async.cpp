0401c29f3a04f8a609995f13a213cf25cf5c9584
sunjiayu <767978963@qq.com>
2024-05-31 12:30:14 +0800

    sync change from remote

--- documentation/audio/examples/create_speech_async.cpp
+++ documentation/audio/examples/create_speech_async.cpp
@@ -5,15 +5,22 @@ using namespace liboai;
 int main() {
   OpenAI oai;
 
-  std::ofstream ocout("demo.mp3", std::ios::binary);
-
   if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
     try {
-      Response res = oai.Audio->speech(
+      std::ofstream ocout("demo.mp3", std::ios::binary);
+      auto fut = oai.Audio->speech_async(
         "tts-1",
         "alloy",
         "Today is a wonderful day to build something people love!"
       );
+      // do other work...
+
+      // check if the future is ready
+      fut.wait();
+
+      // get the contained response
+      auto res = fut.get();
+
       ocout << res.content;
       ocout.close();
       std::cout << res.content.size() << std::endl;
