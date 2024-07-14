23e7c886d17eaccaaa5ae23d4578d87797999fd2
sunjiayu <767978963@qq.com>
2024-07-14 11:19:54 +0800

    change openai root

--- liboai/include/core/network.h
+++ liboai/include/core/network.h
@@ -152,7 +152,7 @@ namespace liboai {
 				return false;
 			}
 			
-			const std::string openai_root_ = "https://mikiai-openai-grutefnkai.us-west-1.fcapp.run";
+			const std::string openai_root_ = "https://mikiai-default-eiaseutzku.us-west-1.fcapp.run";
 			const std::string azure_root_ = ".openai.azure.com/openai";
 
 		private:
